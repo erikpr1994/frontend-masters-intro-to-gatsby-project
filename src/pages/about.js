a42b632a72b6787bc3335d3ac008779684be64d8
Erik <erikpastorrios1994@gmail.com>
2023-08-27 12:42:26 +0200

    Adding links

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,13 @@
 import * as React from 'react';
 
+import { Link } from 'gatsby';
+
 export default function AboutPage() {
   return (
     <main>
       <h1>About this site!</h1>
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <Link to="/">Back to Home</Link>
     </main>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import * as React from 'react';
 
+import { Link } from 'gatsby';
+
 export default function IndexPage() {
   return (
     <main>
       <h1>Welcome to my Gatsby site!</h1>
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <Link to="/about">About page</Link>
     </main>
   );
 }
