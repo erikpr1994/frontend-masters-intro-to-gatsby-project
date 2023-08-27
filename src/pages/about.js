1af4cd10223140a91cd7130f57a1553a2db15373
Erik <erikpastorrios1994@gmail.com>
2023-08-27 12:37:08 +0200

    create pages

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export default function AboutPage() {
+  return (
+    <main>
+      <h1>About this site!</h1>
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    </main>
+  );
+}
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export default function IndexPage() {
+  return (
+    <main>
+      <h1>Welcome to my Gatsby site!</h1>
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    </main>
+  );
+}
